cbc3d53d0e7679fc2539b1fee82b4f53ba0ef7a6
huangkun <huangkuna@gmail.com>
2020-12-09 20:46:17 +0800

    add bar brush

--- mock/bar.ts
+++ mock/bar.ts
@@ -1,17 +1,21 @@
 import { Request, Response } from 'express';
 import { mock } from 'mockjs';
+import { waitTime } from './user';
 
-const getBasicBarData = (req: Request, res: Response) => {
+const getBasicBarData = async (req: Request, res: Response) => {
+  await waitTime(2000);
   res.json(
     mock({
-      "data|26-26": [{
-        "name": /[a-z]/,
-        "value|1-100.1-2": 100
-      }]
-    })
-  )
+      'data|26-26': [
+        {
+          name: /[a-z]/,
+          'value|1-100.1-2': 100,
+        },
+      ],
+    }),
+  );
 };
 
 export default {
-  'GET /api/basic-data': getBasicBarData
-}
+  'GET /api/basic-data': getBasicBarData,
+};
--- src/pages/bar/components/bar-dom.tsx
+++ src/pages/bar/components/bar-dom.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useRef } from 'react';
-import { select, scaleBand, range, scaleLinear, max, axisBottom, axisLeft, brushX } from 'd3';
+import { select, scaleBand, range, scaleLinear, max, axisBottom, axisLeft, brushX, min } from 'd3';
 import { getBasicBarData, BasicBarData } from '@/services/bar';
 import styles from '../index.less';
 
@@ -19,7 +19,7 @@ interface PropsType {
 
 const BarDom: FC<PropsType> = (props) => {
   const { height = 500, width = 950 } = props;
-  const bar = useRef(null);
+  const svg_wrap = useRef(null);
 
   const showToolTip = (event: any, data: { name: string; value: number }) => {
     select('.tooltip')
@@ -31,7 +31,7 @@ const BarDom: FC<PropsType> = (props) => {
 
   const initChart = (data: BasicBarData['data']) => {
     const brushHeight = 90;
-    const svg = select(bar.current);
+    const svg = select(svg_wrap.current);
     const tooltip = select('.tooltip');
     const chart = svg.append('g').attr('class', 'chart');
     const content = svg
@@ -39,31 +39,36 @@ const BarDom: FC<PropsType> = (props) => {
       .attr('class', 'content')
       .attr('transform', `translate(0, ${height - brushHeight})`);
 
-    const Xaxis = scaleBand<number>()
+    const xScale = scaleBand<number>()
       .domain(range(data.length))
       .range([margin.left, width - margin.right])
       .padding(0.1);
 
-    const Yaxis = scaleLinear()
+    const yScale = scaleLinear()
       .domain([0, max(data, (d) => d.value) || 0])
       .nice()
       .range([height - brushHeight - margin.bottom, margin.top]);
 
-    const Yaxis2 = scaleLinear()
+    const xScale2 = scaleBand<number>()
+      .domain(range(data.length))
+      .rangeRound([margin.left, width - margin.right])
+      .padding(0.1);
+
+    const yScale2 = scaleLinear()
       .domain([0, max(data, (d) => d.value) || 0])
       .nice()
       .range([70, 0]);
 
-    chart
+    const bar_rect = chart
       .append('g')
       .attr('fill', color)
       .selectAll('rect')
       .data(data)
       .join('rect')
-      .attr('x', (d, i) => Xaxis(i) as number)
-      .attr('y', (d) => Yaxis(d.value))
-      .attr('height', (d) => Yaxis(0) - Yaxis(d.value))
-      .attr('width', Xaxis.bandwidth())
+      .attr('x', (d, i) => xScale(i) || 0)
+      .attr('y', (d) => yScale(d.value))
+      .attr('height', (d) => yScale(0) - yScale(d.value))
+      .attr('width', xScale.bandwidth())
       .on('mouseover', () => {
         tooltip.style('opacity', 1);
       })
@@ -72,18 +77,19 @@ const BarDom: FC<PropsType> = (props) => {
         tooltip.transition().duration(100).style('opacity', 0);
       });
 
-    chart.append('g').call((g) =>
-      g.attr('transform', `translate(0,${height - brushHeight - margin.bottom})`).call(
-        axisBottom(Xaxis)
-          .tickFormat((i) => data[i].name)
-          .tickSizeOuter(0),
-      ),
-    );
+    const Xaxis = axisBottom(xScale)
+      .tickFormat((i) => data[i].name)
+      .tickSizeOuter(0);
+    const xAxisGroup = chart
+      .append('g')
+      .call((g) =>
+        g.attr('transform', `translate(0,${height - brushHeight - margin.bottom})`).call(Xaxis),
+      );
 
     chart.append('g').call((g) =>
       g
         .attr('transform', `translate(${margin.left}, 0)`)
-        .call(axisLeft(Yaxis).ticks(null, null))
+        .call(axisLeft(yScale).ticks(null, null))
         .call((line) => line.select('.domain').remove())
         .call((text) =>
           text
@@ -99,7 +105,7 @@ const BarDom: FC<PropsType> = (props) => {
     // 数据缩放
     content.append('g').call((g) =>
       g.attr('transform', 'translate(0, 70)').call(
-        axisBottom(Xaxis)
+        axisBottom(xScale)
           .tickFormat((i) => data[i].name)
           .tickSizeOuter(0),
       ),
@@ -111,28 +117,62 @@ const BarDom: FC<PropsType> = (props) => {
       .selectAll('rect')
       .data(data)
       .join('rect')
-      .attr('x', (d, i) => Xaxis(i) || 0)
-      .attr('y', (d) => Yaxis2(d.value))
-      .attr('width', Xaxis.bandwidth())
-      .attr('height', (d) => 70 - Yaxis2(d.value))
+      .attr('x', (d, i) => xScale2(i) || 0)
+      .attr('y', (d) => yScale2(d.value))
+      .attr('width', xScale2.bandwidth())
+      .attr('height', (d) => 70 - yScale2(d.value))
       .attr('fill', color);
+
+    const bar_brush = brushX()
+      .extent([
+        [margin.left, 0.5],
+        [width - margin.right, 70],
+      ])
+      .on('brush', (event) => {
+        if (!event.sourceEvent) return;
+        if (!event.selection) return;
+        const newInput: Array<number> = [];
+        const { selection } = event;
+        xScale2.domain().forEach((i) => {
+          const pos = (xScale2(i) || 0) + xScale2.bandwidth() / 2;
+          if (pos >= selection[0] && pos <= selection[1]) {
+            newInput.push(i);
+          }
+        });
+        xScale.domain(newInput);
+
+        bar_rect
+          .attr('x', (d, i) => xScale(i) || 0)
+          .attr('y', (d) => yScale(d.value))
+          .attr('width', xScale.bandwidth())
+          .attr('height', (d, i) => {
+            if (xScale.domain().indexOf(i) === -1) return 0;
+            return yScale(0) - yScale(d.value);
+          });
+
+        xAxisGroup.call(Xaxis);
+      })
+      .on('end', (event) => {
+        if (!event.sourceEvent) return;
+        if (!event.selection) return;
+        const newInput: Array<number> = [];
+        const { selection } = event;
+        xScale2.domain().forEach((i) => {
+          const pos = (xScale2(i) || 0) + xScale2.bandwidth() / 2;
+          if (pos >= selection[0] && pos <= selection[1]) {
+            newInput.push(i);
+          }
+        });
+        const left = xScale2(min(newInput) || 0);
+        const right = (xScale2(max(newInput) || 0) || 0) + xScale2.bandwidth();
+        select('.brush').transition().call(event.target.move, [left, right]);
+      });
 
     content
       .append('g')
       .attr('class', 'brush')
-      .call(
-        brushX()
-          .extent([
-            [margin.left, 0.5],
-            [width - margin.right, 70],
-          ])
-          .on('brush', (event, d) => {
-            console.log(event, d, 'brush');
-          })
-          .on('end', (event, d) => {
-            console.log(event, d, 'end');
-          }),
-      );
+      .call(bar_brush)
+      .call(bar_brush.move, xScale2.range());
   };
 
   const getData = async () => {
@@ -146,7 +186,7 @@ const BarDom: FC<PropsType> = (props) => {
 
   return (
     <div className={styles.basicBar}>
-      <svg ref={bar} style={{ width, height }} viewBox={`0 0 ${width} ${height}`} />
+      <svg ref={svg_wrap} style={{ width, height }} viewBox={`0 0 ${width} ${height}`} />
       <div className={`tooltip ${styles.tooltip}`} />
     </div>
   );
